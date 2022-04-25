f7ae56d02c28c0e77182fcfc8d9f6c5ae9b4707f
Olaedo Okpareke <olaedo.okpareke@mail.utoronto.ca>
2022-04-25 11:47:36 -0400

    Added the discussion and some results explanations

--- Scripts/Simulation.R
+++ Scripts/Simulation.R
@@ -1,10 +1,14 @@
 
 
 # simulation
+library("tidyverse")
+install.packages("countrycode")
+countries <- countrycode::codelist$country.name.en
 set.seed(438)
-number = 112*1000
+number = 23904
 
 datasim =  tibble(
+  COUNTRYNEW = rep(c(countries), each = 83, replace= FALSE),
   help = sample(c(0,1), size = number, replace = TRUE, prob = c(0.7,0.3)),
   friend = sample(c(0,1), size = number, replace = TRUE, prob = c(0.2,0.8)),
   science = sample(c(0,1), size = number, replace = TRUE, prob = c(0.5,0.5)),
@@ -13,8 +17,7 @@ datasim =  tibble(
   Gender = sample(c(1,2), size = number, replace = TRUE, prob = c(0.5,0.5)),
   Education = sample(c(1:3), size = number, replace = TRUE),
   Income = sample(c(1:4), size = number, replace = TRUE),
-  wbi = sample(c(1:4), size = number, replace = TRUE, prob = c(0.4,0.2,0.2,0.2)),
-
+  wbi = rep(c(1:4), each= number/4, replace = TRUE)
 
 )
 
@@ -22,3 +25,7 @@ datasim =  tibble(
 # dataset used for the report. We use random sampling with observations that are
 # similar to the ones for each variable in the report. We decided to use
 # probability sampling as we don't know the true distribution of these variables.
+
+# weakness: this simulation doesnt account for correlation within the covariates, people
+# in the us have higher wealth than other nations
+# for binary and categorical, simulate propritons as the proprtion you see in the dataset
